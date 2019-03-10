0101ffcdf154ebc55b7367c1d5aaa3e1848173a5
gracieGao <GracieGaor@163.com>
2019-03-10 18:28:34 +0800

    用了MVC

--- js/message.js
+++ js/message.js
@@ -1,49 +1,69 @@
-var APP_ID = 'OfubKUvrAcigGkqWtKv8xxgX-gzGzoHsz';
-var APP_KEY = 'YFHwAulBQlpmYSU41HnFxTa7';
+!function () {
+  var view = document.querySelector('section.message')
 
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
+  var model = {
+    initAV: function () {
+      var APP_ID = 'OfubKUvrAcigGkqWtKv8xxgX-gzGzoHsz'
+      var APP_KEY = 'YFHwAulBQlpmYSU41HnFxTa7'
+      AV.init({ appId: APP_ID, appKey: APP_KEY })
+    },
+    fetch: function () {
+      var query = new AV.Query('Message')//获取并显示历史留言
+      return query.find() //Promise对象
+    },
+    save: function (name, content) {
+      var Message = AV.Object.extend('Message')
+      var message = new Message();// 在表中创建一条数据
+      return message.save({           //Promise对象
+        'name': name,
+        'content': content
+      })
+    }
+  }
 
-var query = new AV.Query('Message')//获取并显示历史留言
-query.find().then(function (messages) {
-  let array = messages.map((item) => item.attributes)
-  array.forEach((item) => {
-    let li = document.createElement('li')
-    li.innerText = `${item.name} : ${item.content}`
-    let messageList = document.querySelector('#messageList')
-    messageList.appendChild(li)
-  })
-})
+  var controller = {
+    view: null,
+    model: null,
+    messageList: null,
+    init: function (view, model) {
+      this.view = view
+      this.model = model
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('form')
+      this.model.initAV()
+      this.loadmessages()
+      this.bindEvents()
+    },
+    loadmessages: function () {
+      this.model.fetch().then
+        ((messages) => {
+          let array = messages.map((item) => item.attributes)
+          array.forEach((item) => {
+            let li = document.createElement('li')
+            li.innerText = `${item.name} : ${item.content}`
+            this.messageList.appendChild(li)
+          })
+        })
+    },
+    bindEvents: function () {
+      this.form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        this.saveMessage()
+      })
+    },
+    saveMessage: function () {
+      let myForm = this.form
+      let content = myForm.querySelector('input[name=content]').value
+      let name = myForm.querySelector('input[name=name]').value
+      this.model.save(name, content).then(function (object) {
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name} : ${object.attributes.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+        myForm.querySelector('input[name=content]').value = ''
+      })
+    }
+  }
+  controller.init(view, model)
 
-let myForm = document.querySelector('#postMessageForm')
-
-myForm.addEventListener('submit', function (e) {
-  e.preventDefault()
-  let content = myForm.querySelector('input[name=content]').value
-  let name = myForm.querySelector('input[name=name]').value
-  console.log(name)
-  var Message = AV.Object.extend('Message')
-  var message = new Message();// 在表中创建一条数据
-  message.save({
-    'name': name,
-    'content': content   //数据内容是words：hello world，保存
-  }).then(function (object) {  //如果保存成功，则运行 alert('')
-    let li = document.createElement('li')
-    li.innerText = `${object.attributes.name} : ${object.attributes.content}`
-    let messageList = document.querySelector('#messageList')
-    messageList.appendChild(li)
-    myForm.querySelector('input[name=content]').value = ''
-
-  })
-})
-
-
-// var TestObject = AV.Object.extend('TestObject');//创建一个TestObject表
-// var testObject = new TestObject();// 在表中创建一条数据
-// testObject.save({
-//   words: 'Hello World!'   //数据内容是words：hello world，保存
-// }).then(function(object) {  //如果保存成功，则运行 alert('')
-//   alert('LeanCloud Rocks!');
-// })
+}.call()
